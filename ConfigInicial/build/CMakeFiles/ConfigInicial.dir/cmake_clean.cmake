c28540a8e0c8d9db6c941e2bba35dbce157303bb
Yael Caballero <yaelcaballero504@gmail.com>
2026-02-19 15:37:55 -0600

    MOD: configuración del proyecto para compilación y ejecución en Linux

---
+++ ConfigInicial/build/CMakeFiles/ConfigInicial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ConfigInicial.dir/link.d"
+  "CMakeFiles/ConfigInicial.dir/Main_P3.cpp.o"
+  "CMakeFiles/ConfigInicial.dir/Main_P3.cpp.o.d"
+  "ConfigInicial"
+  "ConfigInicial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ConfigInicial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
