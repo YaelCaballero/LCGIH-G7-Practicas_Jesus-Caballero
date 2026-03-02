516773ea3a9c9b702c32b3d03734470541892428
Yael Caballero <yaelcaballero504@gmail.com>
2026-03-01 23:19:02 -0600

    ENTREGA: Práctica 4

--- ConfigInicial/Main_Modelado.cpp
+++ ConfigInicial/Main_Modelado.cpp
@@ -1,10 +1,15 @@
 // Caballero Antunez Jesus Yael - 320231364
-// Practica #4: Modelado Geométrico 
-// 24 de febrero del 2026
+// Practica #4: Modelado Geométrico
+// 1 de marzo del 2026
 
 #include <cmath>
+#include <cstdio>
+#include <glm/detail/qualifier.hpp>
+#include <glm/ext/matrix_float4x4.hpp>
 #include <glm/ext/matrix_transform.hpp>
+#include <glm/ext/scalar_constants.hpp>
 #include <glm/ext/vector_float3.hpp>
+#include <glm/trigonometric.hpp>
 #include <iostream>
 
 // #define GLEW_STATIC
@@ -39,7 +44,7 @@ int main() {
   glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
   GLFWwindow *window = glfwCreateWindow(
-      WIDTH, HEIGHT, "Previo 4 Jesús Caballero", nullptr, nullptr);
+      WIDTH, HEIGHT, "Práctica 4 Jesús Caballero", nullptr, nullptr);
 
   int screenWidth, screenHeight;
 
@@ -160,6 +165,9 @@ int main() {
     GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
     GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
     GLint projecLoc = glGetUniformLocation(ourShader.Program, "projection");
+    GLint colorLoc = glGetUniformLocation(ourShader.Program, "overrideColor");
+    GLint useOverrideLoc =
+        glGetUniformLocation(ourShader.Program, "useOverride");
 
     glUniformMatrix4fv(projecLoc, 1, GL_FALSE, glm::value_ptr(projection));
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -167,61 +175,173 @@ int main() {
 
     glBindVertexArray(VAO);
 
-    // Losa
+    // Suelo
+    glUniform1i(useOverrideLoc, GL_TRUE);
+    glUniform3f(colorLoc, 0.0f, 0.5f, 0.0f);
+  
     model = glm::mat4(1.0f);
-    glm::vec3 losa_dim, pata_dim;
 
-    losa_dim = glm::vec3(4.0f, 0.2f, 2.0f);
-    pata_dim = glm::vec3(0.2f, 1.0f, 0.2f);
+    model = glm::translate(model, glm::vec3(0.0f, -0.5f, 0.0f));
+    model = glm::scale(model, glm::vec3(40.0f, 1.0f, 40.0f));
 
-    model = glm::translate(model,
-                           glm::vec3(0.0f, losa_dim.y / 2 + pata_dim.y, 0.0f));
-    model = glm::scale(model, losa_dim);
 
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
-    // Pata 1
+    // Base
     model = glm::mat4(1.0f);
 
-    float mag = std::sqrt(pow(-losa_dim.z + pata_dim.z, 2) +
-                          pow(losa_dim.x - pata_dim.x, 2)) /
-                2;
-    float ang = atan2(-losa_dim.z + pata_dim.z, losa_dim.x - pata_dim.x);
+    glm::vec3 base_dim = glm::vec3(10.0f, 1.0f, 10.0f);
+    glm::vec3 base_pos = glm::vec3(0.0f, base_dim.y / 2, 0.0f);
+    float reduccion = base_dim.x / 9;
+    int pisos = 7;
 
-    model = glm::translate(
-        model, glm::vec3(mag * cos(ang), pata_dim.y / 2, mag * sin(ang)));
-    model = glm::scale(model, pata_dim);
+    // Pisos
+    for (int i = 0; i < pisos; i++) {
+      float tonoPiso = 0.5f + (i * 0.05f);
+      glUniform3f(colorLoc, tonoPiso, tonoPiso, tonoPiso);
+
+      model = glm::mat4(1.0f);
+
+      model = glm::translate(
+          model,
+          glm::vec3(base_pos.x, base_pos.y + i * base_dim.y, base_pos.z));
+      model =
+          glm::scale(model, glm::vec3(base_dim.x - reduccion * i, base_dim.y,
+                                      base_dim.z - reduccion * i));
+
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    float altura = base_dim.y * pisos;
+    float angle;
+
+    // Escaleras
+    glUniform3f(colorLoc, 0.4f, 0.4f, 0.4f);
+    glm::vec3 escalera_dim = glm::vec3();
+    glm::vec3 escalera_pos = glm::vec3();
+
+    angle = atan2(reduccion / 2.0f, base_dim.y);
+
+    escalera_dim.x = base_dim.x - reduccion * (pisos + 1);
+    escalera_dim.y = altura / cos(angle);
+    escalera_dim.z = 0.5f;
+
+    escalera_pos.x = 0;
+    escalera_pos.y =
+        (escalera_dim.y * cos(angle) / 2 -
+         escalera_dim.z *
+             (base_dim.z + reduccion - escalera_dim.y * cos(angle)) /
+             (2 * escalera_dim.y));
+    escalera_pos.z =
+        ((base_dim.z + reduccion - escalera_dim.y * sin(angle)) / 2 -
+         escalera_dim.z * escalera_pos.y / escalera_dim.y);
+
+    for (int i = 0; i < 4; i++) {
+      model = glm::mat4(1.0f);
+
+      model = glm::rotate(model, glm::radians(90.0f * i),
+                          glm::vec3(0.0f, 1.0f, 0.0f));
+      model = glm::translate(model, escalera_pos);
+      model = glm::rotate(model, angle, glm::vec3(-1.0f, 0.0f, 0.0f));
+      model = glm::scale(model, escalera_dim);
+
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    // Columnas - Coords polares
+    glUniform3f(colorLoc, 0.85f, 0.85f, 0.85f);
+    glm::vec3 columna_dim1 = glm::vec3(0.75f, 1.25f, 0.2f);
+    glm::vec3 columna_dim2 = glm::vec3(columna_dim1.z, columna_dim1.y,
+                                       columna_dim1.x - columna_dim1.z);
+
+    glm::vec3 columna_pos1 = glm::vec3();
+    glm::vec3 columna_pos2 = glm::vec3();
+
+    columna_pos1.y = altura + columna_dim1.y / 2;
+    columna_pos2.y = columna_pos1.y;
+
+    float mag1 = sqrt(pow(base_dim.x - reduccion * pisos - columna_dim1.x, 2) +
+                      pow(base_dim.z - reduccion * pisos - columna_dim1.z, 2)) /
+                 2;
+    float angle1 = atan2(base_dim.z - reduccion * pisos - columna_dim1.z,
+                         base_dim.x - reduccion * pisos - columna_dim1.x);
+
+    float mag2 =
+        sqrt(pow((base_dim.x - reduccion * pisos - columna_dim2.x) / 2, 2) +
+             pow((base_dim.z - reduccion * pisos - columna_dim2.z) / 2 -
+                     columna_dim1.z,
+                 2));
+    float angle2 = atan2((base_dim.z - reduccion * pisos - columna_dim2.z) / 2 -
+                             columna_dim1.z,
+                         (base_dim.x - reduccion * pisos - columna_dim2.x) / 2);
+
+    for (int i = 0; i < 4; i++) {
+      // Columnas
+      model = glm::mat4(1.0f);
+
+      columna_pos1.x = pow(-1, i / 2) * mag1 * cos(angle1);
+      columna_pos1.z = pow(-1, i) * mag1 * sin(angle1);
+
+      model = glm::translate(model, columna_pos1);
+      model = glm::scale(model, columna_dim1);
+
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+
+      model = glm::mat4(1.0f);
+
+      columna_pos2.x = pow(-1, i / 2) * mag2 * cos(angle2);
+      columna_pos2.z = pow(-1, i) * mag2 * sin(angle2);
+
+      model = glm::translate(model, columna_pos2);
+      model = glm::scale(model, columna_dim2);
+
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
+
+    altura += columna_dim1.y;
+
+    // Techo
+    glUniform3f(colorLoc, 0.55f, 0.55f, 0.55f);
+    model = glm::mat4(1.0f);
+
+    model =
+        glm::translate(model, glm::vec3(0.0f, altura + base_dim.y / 6, 0.0f));
+    model = glm::scale(model, glm::vec3(base_dim.x - reduccion * pisos + 0.3f,
+                                        base_dim.y / 3,
+                                        base_dim.x - reduccion * pisos + 0.3f));
 
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
-    // Pata 2
+    altura += base_dim.y / 3;
+
+    glUniform3f(colorLoc, 0.85f, 0.85f, 0.85f);
     model = glm::mat4(1.0f);
 
-    model = glm::translate(
-        model, glm::vec3(-mag * cos(ang), pata_dim.y / 2, mag * sin(ang)));
-    model = glm::scale(model, pata_dim);
+    model =
+        glm::translate(model, glm::vec3(0.0f, altura + base_dim.y / 4, 0.0f));
+    model = glm::scale(model,
+                       glm::vec3(base_dim.x - reduccion * pisos, base_dim.y / 2,
+                                 base_dim.x - reduccion * pisos));
 
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
-    // Pata 3
+    altura += base_dim.y / 2;
+
+    glUniform3f(colorLoc, 0.55f, 0.55f, 0.55f);
     model = glm::mat4(1.0f);
 
-    model = glm::translate(
-        model, glm::vec3(-mag * cos(ang), pata_dim.y / 2, -mag * sin(ang)));
-    model = glm::scale(model, pata_dim);
-
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-
-    // Pata 4
-    model = glm::mat4(1.0f);
-
-    model = glm::translate(
-        model, glm::vec3(mag * cos(ang), pata_dim.y / 2, -mag * sin(ang)));
-    model = glm::scale(model, pata_dim);
+    model =
+        glm::translate(model, glm::vec3(0.0f, altura + base_dim.y / 6, 0.0f));
+    model = glm::scale(model, glm::vec3(base_dim.x - reduccion * pisos + 0.3f,
+                                        base_dim.y / 3,
+                                        base_dim.x - reduccion * pisos + 0.3f));
 
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glDrawArrays(GL_TRIANGLES, 0, 36);
@@ -255,7 +375,7 @@ void Inputs(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
     movZ -= 0.08f;
   if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-    rot += 0.4f;
+    rot -= 0.8f;
   if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-    rot -= 0.4f;
+    rot += 0.8f;
 }
--- ConfigInicial/Shader/core.frag
+++ ConfigInicial/Shader/core.frag
@@ -1,9 +1,14 @@
 #version 330 core
+out vec4 color;
 in vec3 ourColor;
 
-out vec4 color;
+uniform vec3 overrideColor; 
+uniform bool useOverride;   
 
-void main()
-{
-	color = vec4(ourColor, 1.0f);
+void main() {
+    if(useOverride) {
+        color = vec4(overrideColor, 1.0f);
+    } else {
+        color = vec4(ourColor, 1.0f);
+    }
 }
